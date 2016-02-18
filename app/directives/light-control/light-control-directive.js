3b63db5b932e640c32e961b198b73e07f3a3152d
Martin Randall <martin@coryl.com>
2016-02-18 17:32:34 +0000

    Rudamentary disco mode implemented

--- app/directives/light-control/light-control-directive.js
+++ app/directives/light-control/light-control-directive.js
@@ -4,9 +4,9 @@
 
     mod.directive("lightControl", lightControl);
 
-    lightControl.$inject = ['lightControlService'];
+    lightControl.$inject = ['lightControlService', '$interval'];
 
-    function lightControl(lightControlService) {
+    function lightControl(lightControlService, $interval) {
         return {
             restrict: "E",
             templateUrl: "app/directives/light-control/light-control-template.html",
@@ -14,6 +14,8 @@
                 light: '='
             },
             link: function (scope) {
+                scope.discoMode = false;
+
                 scope.$watch('light.levels.red', function (newValue) {
                     lightControlService.setLight(scope.light.deviceIds.red, scaleValue(newValue));
                 });
@@ -26,9 +28,27 @@
                 scope.$watch('light.levels.white', function (newValue) {
                     lightControlService.setLight(scope.light.deviceIds.white, scaleValue(newValue));
                 });
+                scope.$watch('discoMode', function(newValue) {
+                    setDisco(newValue);
+                });
+
+                function setDisco(discoOn) {
+                    console.log("Disco: " + discoOn);
+
+
+                    if (discoOn) {
+                        $interval(function() {
+                            scope.light.levels.red = 255 - scope.light.levels.red;
+                        }, 1000);
+                    }
+                }
+
             }
         };
+
+
     }
+
 
     /**
      * Scales a 0-255 value to 0-100
